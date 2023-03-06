d0138c0629e297059f09e4117ae3f73a2ddd53c6
Ilnur <ilnur.stybayev@kaspi.kz>
2023-03-06 20:52:30 +0600

    tab_layout_with_view_pager2

--- app/src/main/java/com/example/myapplication/NinthActivity.kt
+++ app/src/main/java/com/example/myapplication/NinthActivity.kt
@@ -2,7 +2,11 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.TabLayoutOnPageChangeListener
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_ninth.*
 import kotlinx.android.synthetic.main.activity_tenth.*
 
@@ -21,6 +25,25 @@ class NinthActivity : AppCompatActivity() {
         val adapter = ViewPagerAdapter(images)
         viewPager.adapter = adapter
         viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        TabLayoutMediator(tabLayout,viewPager){tab, position ->
+            tab.text = "Tab ${position + 1}"
+        }.attach()
+
+        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@NinthActivity,"Selected ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@NinthActivity,"Unselected ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@NinthActivity,"Reselected ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+        })
+
 
         btnBackNineActivity.setOnClickListener {
             finish()
