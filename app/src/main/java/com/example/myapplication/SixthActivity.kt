c0d47641259da0ce126f5378396caf3672b9c457
Ilnur <ilnur.stybayev@kaspi.kz>
2023-03-07 23:41:17 +0600

    NOTIFICATIONS

--- app/src/main/java/com/example/myapplication/SixthActivity.kt
+++ app/src/main/java/com/example/myapplication/SixthActivity.kt
@@ -1,17 +1,55 @@
 package com.example.myapplication
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import kotlinx.android.synthetic.main.activity_sixth.*
 import kotlinx.android.synthetic.main.custom_toast.*
 
 @Suppress("DEPRECATION")
 class SixthActivity : AppCompatActivity() {
+
+    val CHANNEL_ID = "channelID"
+    val CHANNEL_NAME = "chanelNAME"
+    val NOTIFICATION_ID = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sixth)
-        btnShowTost.setOnClickListener {
+
+        createNotificationChannel()
+
+        val intent = Intent(this, SixthActivity::class.java)
+        val pandingIntent = TaskStackBuilder.create(this).run{
+            addNextIntentWithParentStack(intent)
+            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        }
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Awesome notification")
+            .setContentText("This is the content text")
+            .setSmallIcon(R.drawable.ic_star)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pandingIntent)
+            .build()
+
+        val notificationManager = NotificationManagerCompat.from(this)
+
+        btnShowNotifications.setOnClickListener {
+            notificationManager.notify(NOTIFICATION_ID, notification)
+        }
+
+        btnShowToast.setOnClickListener {
             Toast(this).apply {
                 duration = Toast.LENGTH_LONG
                 view = layoutInflater.inflate(R.layout.custom_toast, clToast)
@@ -20,6 +58,18 @@ class SixthActivity : AppCompatActivity() {
         }
         btnBack6.setOnClickListener {
             finish()
+        }
+    }
+
+    fun createNotificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(CHANNEL_ID,CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT).apply {
+                    lightColor = Color.GREEN
+                enableLights(true)
+            }
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
         }
     }
 }
